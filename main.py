1887c9b629b7e531a3882439b4a4c4a78954df4a
Fadil Bantan <fadooly12@gmail.com>
2025-07-20 20:39:10 +0300

    function to show number of characters, correctly returning

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
-from stats import get_num_words
+from stats import get_num_words, get_char_count
 
-# function to open the book using a filepath
+# function to open (read) the book using a filepath
 def get_book_text(filepath):
     with open(filepath) as f:
         return f.read()
 
-# main to return called function
+# main to return called functions
 def main():
-    # return print(get_book_text("books/frankenstein.txt"))
-    return print(f"{get_num_words("books/frankenstein.txt")} words found in the document")
+    # Get the word count
+    word_count = get_num_words(get_book_text, "books/frankenstein.txt")
+    print(f"{word_count} words found in the document")
+    
+    # Get the character count
+    char_count = get_char_count(get_book_text, "books/frankenstein.txt")
+    print(f"Character counts: {char_count}")
 
 main()
--- stats.py
+++ stats.py
@@ -1,5 +1,13 @@
 # function to calculate the number of words in the book
-def get_num_words(filepath):
-    with open(filepath) as f:
-        book_content = f.read()
-        return len(book_content.split())
+def get_num_words(get_book_text, filepath):
+    num_words = len(get_book_text(filepath).split())
+    return num_words
+
+# print in a dictionary each character's count
+def get_char_count(get_book_text, filepath):
+    text = tuple(get_book_text(filepath))
+    char_count = {}
+    for char in text:
+        lowercase_char = char.lower()
+        char_count[lowercase_char] = char_count.get(lowercase_char, 0) + 1
+    return char_count
